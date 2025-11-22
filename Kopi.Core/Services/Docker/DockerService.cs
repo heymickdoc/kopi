09d6ea27cdab947fc60bfd9e898c46cc663961d0
heymickdoc <mick@27b.uk>
2025-11-22 17:31:13 +0000

    Refactored the kopi down functionality

--- Kopi.Core/Services/Docker/DockerService.cs
+++ Kopi.Core/Services/Docker/DockerService.cs
@@ -338,4 +338,54 @@ public class DockerService
 
         return allContainersStatus;
 	}
+
+    /// <summary>
+    ///  Tears down the specified Docker container.
+    /// </summary>
+    /// <param name="containerName">The container to tear down</param>
+    public static async Task ExecuteTearDown(string containerName)
+    {
+        var doesContainerExist = await DoesContainerExist(containerName);
+
+        if (!doesContainerExist)
+        {
+            Msg.Write(MessageType.Info,
+                $"Could not find Docker container \"{containerName}\" to tear down. Exiting.");
+        }
+        else
+        {
+            var stoppedAndDeleted = await StopAndDeleteRunningContainer(containerName);
+
+            if (stoppedAndDeleted)
+            {
+                Msg.Write(MessageType.Success, $"Docker container {containerName} down");
+            }
+            else
+            {
+                Msg.Write(MessageType.Error, $"Failed to tear down the Docker container {containerName}");
+            }
+        }
+    }
+    
+    public static async Task<bool> ExecuteTearDownAll(List<string> containers)
+    {
+        var isErrors = false;
+
+        foreach (var container in containers)
+        {
+            var stoppedAndDeleted = await StopAndDeleteRunningContainer(container);
+            if (stoppedAndDeleted)
+            {
+                Msg.Write(MessageType.Success, $"Docker container {container} torn down successfully.");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Msg.Write(MessageType.Error, $"Failed to tear down the Docker container {container}");
+                isErrors = true;
+            }
+        }
+
+        return isErrors;
+    }
 }
