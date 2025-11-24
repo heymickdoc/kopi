1546941fb34cb9df9f2b8c35457f2ffd5aa58d30
heymickdoc <mick@27b.uk>
2025-11-24 15:19:38 +0000

    Massive refactor of DataOrchestrator

---
+++ Kopi.Core/Interfaces/ITableDataStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Kopi.Core.Models.SQLServer;
+
+namespace Kopi.Core.Interfaces;
+
+public interface ITableDataStrategy
+{
+    /// <summary>
+    /// Generates (or retrieves) the data rows for a specific table.
+    /// </summary>
+    /// <param name="table">The table metadata.</param>
+    /// <param name="maxRowCount">The calculated row cap.</param>
+    /// <param name="existingData">Data generated for previous tables (needed for Foreign Keys).</param>
+    Task<List<RowData>> GenerateTableDataAsync(
+        TableModel table, 
+        int maxRowCount, 
+        List<TargetDataModel> existingData);
+}
